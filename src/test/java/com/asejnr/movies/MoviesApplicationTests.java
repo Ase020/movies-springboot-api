fd534f10195509943892e762ed2aca95508f9673
felixNyalenda <felixasejunior@gmail.com>
2024-02-15 10:00:29 +0300

    init commit

---
+++ src/test/java/com/asejnr/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asejnr.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
